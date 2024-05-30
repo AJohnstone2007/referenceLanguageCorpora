8f166f3ed7e3d8d40d56cd5f40baa5ecb9627b27
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2024-05-30 08:26:33 +0100

    Update Java and SML with later grammars

---
+++ java/jls18Grammar/extractJLS18.bat
@@ -0,0 +1,5 @@
+call %arthome%/art !extractJLS JLS18CharacterClassRules.raw jls18_sec3_FromHTML.raw jls18_sec19_FromHTML jls18Base CompilationUnit
+
+copy jls18Base.art + jls18LexCharacters.art jls18.art
+
+jls18.art
