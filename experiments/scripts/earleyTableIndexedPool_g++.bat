39a91c4c34fac2310c72018d867aa7ef8dda16a4
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2024-08-23 21:31:17 +0100

    Add ./ to executabel call in .bat scripts for Un*x

--- experiments/scripts/earleyTableIndexedPool_g++.bat
+++ experiments/scripts/earleyTableIndexedPool_g++.bat
@@ -1,4 +1,4 @@
 java -jar tools/art/art.jar v3 %1 !earleyTableIndexedData
 g++ -O3 -I. -Itools/earlyFromARTV3InCpp -oARTEarleyTableIndexedPool tools/earlyFromARTV3InCpp/ARTEarleyTableIndexedPool.cpp
-ARTEarleyTableIndexedPool %2
+./ARTEarleyTableIndexedPool %2
 
--- experiments/try/scripts/earleyIndexedPool_g++.bat
+++ experiments/try/scripts/earleyIndexedPool_g++.bat
@@ -1,4 +1,4 @@
 java -jar tools/art/art.jar v3 %1 !earleyIndexedData
 g++ -O3 -I. -Itools/earlyFromARTV3InCpp -oARTEarleyIndexedPool tools/earlyFromARTV3InCpp/ARTEarleyIndexedPool.cpp
-ARTEarleyIndexedPool %2
+./ARTEarleyIndexedPool %2
 
